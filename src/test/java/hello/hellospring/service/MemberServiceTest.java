5ff833912350412c406d5e825464505d3c07d24d
aammddkkzxc <ideal0427@naver.com>
2024-03-12 01:00:55 +0900

    refactor: 멤버 레포지토리 테스트 통합테스트로

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,24 +1,25 @@
 package hello.hellospring.service;
 
-import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 class MemberServiceTest {
+    @Autowired
     MemberService memberService;
-    MemoryMemberRepository memberRepository;
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    } @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+
+    @Autowired
+    MemberRepository memberRepository;
+
     @Test
     public void 회원가입() {
         //Given
@@ -30,6 +31,7 @@ class MemberServiceTest {
         Member findMember = memberRepository.findById(saveId).get();
         assertEquals(member.getName(), findMember.getName());
     }
+
     @Test
     public void 중복_회원_예외() {
         //Given
